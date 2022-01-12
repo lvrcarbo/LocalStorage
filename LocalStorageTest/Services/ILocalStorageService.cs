2086818b8848c8fdb46c3b7dc637eee9776d0b5e
Alvaro Carbo <lvrcarbo@gmail.com>
2022-01-12 13:13:41 +0100

    Chapter 4 DONE

---
+++ LocalStorageTest/Services/ILocalStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalStorageTest.Services
+{
+    public interface ILocalStorageService
+    {
+        Task SetItemAsync<T>(string key, T item);
+        Task<T> GetItemAsync<T>(string item);
+    }
+}
